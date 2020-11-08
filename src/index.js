b69d571fac172e3ac8fbe9dae5c7f25728dad446
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-11-08 05:19:38 -0500

    First react app of showing greeting

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Greeting() {
+  return <h4>This is Umesh and this is my first component</h4>;
+}
+
+ReactDOM.render(<Greeting />, document.getElementById("root"));
