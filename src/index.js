52efc14fcb73ac7900d32d233e53f51472d374dc
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-11-08 06:53:21 -0500

    Example of what is happening under-the-hood

--- src/index.js
+++ src/index.js
@@ -5,4 +5,12 @@ function Greeting() {
   return <h4>This is Umesh and this is my first component</h4>;
 }
 
+// const Greeting = () => {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h1", {}, "hello world")
+//   );
+// };
+
 ReactDOM.render(<Greeting />, document.getElementById("root"));
