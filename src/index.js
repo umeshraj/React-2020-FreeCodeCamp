e5a682457d6ef90a7c7229f87fd20cccb4e51a31
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-11-10 04:58:14 -0500

    Spring clearning of components

--- src/index.js
+++ src/index.js
@@ -8,10 +8,6 @@ function BookList() {
   return (
     <section className="booklist">
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
@@ -19,26 +15,15 @@ function BookList() {
 const Book = () => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
+        alt=""
+      />
+      <h1>A Wealth of Pigeons: A Cartoon Collection</h1>
+
+      <h4>Steve Martin</h4>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
-    alt=""
-  />
-);
-
-const Title = () => <h1>A Wealth of Pigeons: A Cartoon Collection</h1>;
-
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-    Steve Martin
-  </h4>
-);
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
