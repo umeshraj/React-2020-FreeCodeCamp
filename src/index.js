0794d377dff8b2136302acb51af4be9ab2b1fa7c
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-11-11 05:36:27 -0500

    Using spread operator instead of creating props.book

--- src/index.js
+++ src/index.js
@@ -33,14 +33,14 @@ function BookList() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book key={book.id} book={book}></Book>;
+        return <Book key={book.id} {...book}></Book>;
       })}
     </section>
   );
 }
 
 const Book = (props) => {
-  const { img, title, author } = props.book;
+  const { img, title, author } = props;
   return (
     <article className="book">
       <img src={img} alt="" />
