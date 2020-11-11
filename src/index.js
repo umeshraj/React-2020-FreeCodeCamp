266ae9d20f6744e46cf60f817a4f54b9ce1aa2d0
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-11-10 21:18:21 -0500

    Using props

--- src/index.js
+++ src/index.js
@@ -4,27 +4,30 @@ import ReactDOM from "react-dom";
 // CSS
 import "./index.css";
 
+// CONSTANTS
+const title = "A Wealth of Pigeons: A Cartoon Collection";
+const author = "Steve Martin";
+const img =
+  "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg";
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book job="developer" />
+      <Book title="random title" number={22} />
     </section>
   );
 }
 
-const Book = () => {
-  const title = "A Wealth of Pigeons: A Cartoon Collection";
-  const author = "Steve Martin";
+const Book = (props) => {
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
-        alt=""
-      />
+      <img src={img} alt="" />
       <h1>{title}</h1>
-
       <h4>{author}</h4>
-      <p>{6 + 6}</p>
+      <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p>
     </article>
   );
 };
