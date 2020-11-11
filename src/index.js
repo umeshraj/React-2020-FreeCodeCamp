6f89eceaf969a8bf42102c96db382a75a2bdf6af
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-11-11 05:32:15 -0500

    Adding key prop for lists

--- src/index.js
+++ src/index.js
@@ -7,18 +7,21 @@ import "./index.css";
 // Vars
 const books = [
   {
+    id: 1,
     img:
       "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
     title: "A Wealth of Pigeons: A Cartoon Collection",
     author: "Steve Martin",
   },
   {
+    id: 2,
     img:
       "https://images-na.ssl-images-amazon.com/images/I/81GeAcdMCsL._AC_UL200_SR200,200_.jpg",
     title: "Humans",
     author: "Brandon Stanton",
   },
   {
+    id: 3,
     img:
       "https://images-na.ssl-images-amazon.com/images/I/71Ks%2B8mKq2L._AC_UL200_SR200,200_.jpg",
     title: "The Deep End (Diary of a Wimpy Kid Book 15)",
@@ -30,7 +33,7 @@ function BookList() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book book={book}></Book>;
+        return <Book key={book.id} book={book}></Book>;
       })}
     </section>
   );
