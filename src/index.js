7bae2ab56f8a2032c8c85f8b5c822d09b0a3c581
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-11-12 05:27:02 -0500

    onClick event handlers

--- src/index.js
+++ src/index.js
@@ -41,11 +41,24 @@ function BookList() {
 
 const Book = (props) => {
   const { img, title, author } = props;
+  const clickHandler = (e) => {
+    // alert("Hello, world");
+    console.log(e.target);
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
   return (
     <article className="book">
       <img src={img} alt="" />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        Reference Example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        More complex example
+      </button>
     </article>
   );
 };
