1f865098bf7bb80336a09e141bf865f701c81e00
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-11-13 05:36:12 -0500

    useState to change title on button click

--- part2_advanced_intro/src/App.js
+++ part2_advanced_intro/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/setup/1-error-example";
+import Setup from "./tutorial/1-useState/setup/2-useState-basics";
 // import Final from "./tutorial/1-useState/final/1-error-example";
 
 function App() {
