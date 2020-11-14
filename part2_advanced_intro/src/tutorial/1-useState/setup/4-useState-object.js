61fca5c7db32d79797674affe3da8c733160e431
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-11-14 10:33:29 -0500

    Multiple useState

--- part2_advanced_intro/src/tutorial/1-useState/setup/4-useState-object.js
+++ part2_advanced_intro/src/tutorial/1-useState/setup/4-useState-object.js
@@ -8,14 +8,20 @@ const UseStateObject = () => {
   });
 
   const changeMessage = () => {
-    setPerson({ ...person, message: "hello, world" });
+    // setPerson({ ...person, message: "hello, world" });
+    setMessage("Hello world");
   };
+
+  // use state for each key
+  const [name, setName] = useState("peter");
+  const [age, setAge] = useState(25);
+  const [message, setMessage] = useState("random message");
 
   return (
     <>
-      <h3>{person.name}</h3>
-      <h3>{person.age}</h3>
-      <h3>{person.message}</h3>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
       <button className="btn" onClick={changeMessage}>
         Change message
       </button>
