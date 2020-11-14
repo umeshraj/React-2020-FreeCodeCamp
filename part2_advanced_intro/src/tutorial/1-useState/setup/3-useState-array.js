13d9cd30beda02d9e8a7c9c081a40c5f6d22a8f7
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-11-14 10:00:23 -0500

    Removing all items w/ useState

--- part2_advanced_intro/src/tutorial/1-useState/setup/3-useState-array.js
+++ part2_advanced_intro/src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+
+        return (
+          <div key={id} className="item">
+            {<h4>{name}</h4>}
+          </div>
+        );
+      })}
+      <button className="btn" onClick={() => setPeople([])}>
+        Clear items
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
