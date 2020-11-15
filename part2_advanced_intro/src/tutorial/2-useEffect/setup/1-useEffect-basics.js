9fa417be5c243307520aef58235f76a1bce95f79
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-11-15 06:04:28 -0500

    Use effect with dependency list

--- part2_advanced_intro/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ part2_advanced_intro/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
-};
+  const [value, setValue] = useState(0);
 
+  useEffect(() => {
+    console.log("call useEffect");
+    if (value > 0) {
+      document.title = `New Messages(${value})`;
+    }
+  }, [value]);
+
+  // 2nd useEffect to run only once
+  useEffect(() => {
+    console.log("Hello, World!");
+  }, []);
+
+  console.log("render component");
+  return (
+    <>
+      <h1>{value}</h1>
+      <button
+        className="btn"
+        onClick={() => {
+          setValue(value + 1);
+        }}
+      >
+        Click me
+      </button>
+    </>
+  );
+};
 export default UseEffectBasics;
