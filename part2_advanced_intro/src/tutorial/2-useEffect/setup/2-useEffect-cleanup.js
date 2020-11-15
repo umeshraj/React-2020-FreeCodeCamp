0362f29ecb634206e99c639fe9a9fea9e85ed39f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-11-15 06:18:15 -0500

    Clean up fn in useEffect

--- part2_advanced_intro/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ part2_advanced_intro/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => {
+      console.log("clean up");
+      window.removeEventListener("resize", checkSize);
+    };
+  });
+
+  return (
+    <>
+      <h1>window</h1>
+      <h2>{size}Px</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
