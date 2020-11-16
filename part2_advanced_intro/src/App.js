2dc387dfa965073745bce74565447b1b7ab64732
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-11-16 05:46:44 -0500

    Short circuit operators

--- part2_advanced_intro/src/App.js
+++ part2_advanced_intro/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
+import Setup from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
 // import Final from "./tutorial/1-useState/final/1-error-example";
 
 function App() {
--- part2_advanced_intro/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ part2_advanced_intro/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("Umesh");
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>value: {secondValue}</h1> */}
+      <h1>{text || "John Doe"}</h1>
+      {text && <h1>{"Hello world"}</h1>}
+    </>
+  );
 };
 
 export default ShortCircuit;
