6eadc55f204669fe68204a098c7f2d85e94d6635
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-11-17 05:21:35 -0500

    Basic forms in React

--- part2_advanced_intro/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ part2_advanced_intro/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,7 +6,31 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Hello, world!");
+  };
+  return (
+    <>
+      <article>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="firstName">Name: </label>
+            <input type="text" name="firstName" id="firstName" />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="email">Email: </label>
+            <input type="text" name="email" id="email" />
+          </div>
+
+          <button type="submit" onClick={handleSubmit}>
+            Add person
+          </button>
+        </form>
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
