cd22a9811f33e7dc685bc6e4faae838569fb11cc
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-11-17 05:30:49 -0500

    Setting up onchange for form

--- part2_advanced_intro/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ part2_advanced_intro/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,6 +6,9 @@ import React, { useState } from "react";
 // value, onChange
 
 const ControlledInputs = () => {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Hello, world!");
@@ -16,12 +19,28 @@ const ControlledInputs = () => {
         <form className="form" onSubmit={handleSubmit}>
           <div className="form-control">
             <label htmlFor="firstName">Name: </label>
-            <input type="text" name="firstName" id="firstName" />
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              value={firstName}
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+            />
           </div>
 
           <div className="form-control">
             <label htmlFor="email">Email: </label>
-            <input type="text" name="email" id="email" />
+            <input
+              type="text"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
           </div>
 
           <button type="submit" onClick={handleSubmit}>
