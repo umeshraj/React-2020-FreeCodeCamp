d4ef7cc5f1b9df13553622c89be919f3ca0fc75b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-11-18 05:46:22 -0500

    Create list of people from form input

--- part2_advanced_intro/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ part2_advanced_intro/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -8,10 +8,19 @@ import React, { useState } from "react";
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState("");
   const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Hello, world!");
+    if (firstName && email) {
+      const person = { id: new Date().getTime().toString(), firstName, email };
+      setPeople((people) => [...people, person]);
+      // reset vars
+      setFirstName("");
+      setEmail("");
+    } else {
+      console.log("empty values");
+    }
   };
   return (
     <>
@@ -47,6 +56,16 @@ const ControlledInputs = () => {
             Add person
           </button>
         </form>
+        {/* show list on page */}
+        {people.map((person) => {
+          const { id, firstName, email } = person;
+          return (
+            <div className="item" key={id}>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </article>
     </>
   );
