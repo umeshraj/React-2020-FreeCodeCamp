b878d22086e3f50f87ee771081da91b10e7c0a1e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-11-21 15:22:31 -0500

    Move reducer to a different file

--- part2_advanced_intro/src/tutorial/6-useReducer/setup/index.js
+++ part2_advanced_intro/src/tutorial/6-useReducer/setup/index.js
@@ -1,35 +1,9 @@
 import React, { useState, useReducer } from "react";
 import Modal from "./Modal";
 import { data } from "../../../data";
-
 // reducer function
-const reducer = (state, action) => {
-  if (action.type === "ADD_ITEM") {
-    const newPeople = [...state.people, action.payload];
-    return {
-      ...state,
-      people: newPeople,
-      isModalOpen: true,
-      modalContent: "item added",
-    };
-  }
-  if (action.type === "NO_VALUE") {
-    return { ...state, isModalOpen: true, modalContent: "Please enter value" };
-  }
+import { reducer } from "./reducer";
 
-  if (action.type === "CLOSE_MODAL") {
-    return { ...state, isModalOpen: false };
-  }
-
-  if (action.type === "REMOVE_ITEM") {
-    const newPeople = state.people.filter(
-      (person) => person.id !== action.payload
-    );
-    return { ...state, people: newPeople };
-  }
-
-  throw new Error("no matching action type");
-};
 const defaultState = {
   people: [],
   isModalOpen: false,
