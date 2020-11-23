8a53deccfb2c4e2f387b5f761b0e60e2083a0faf
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-11-23 04:52:27 -0500

    Custom hook to fetch data

--- part2_advanced_intro/src/App.js
+++ part2_advanced_intro/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/8-useContext/setup/1-context-api";
+import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
 // import Final from "./tutorial/7-prop-drilling/final/1-prop-drilling";
 
 function App() {
